cb0e6338b9055429d3bef0be04849a8b5fa9b5ce
Grigor Poghosyan <grigor.poghosyan@goya.com>
2022-11-02 17:03:06 -0400

    Test

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
-  cloud {
+  backend "remote" {
+    hostname = "app.terraform.io"
     organization = "publicsq-test1234"
 
     workspaces {
       name = "learn-terraform-circleci"
+      prefix = "my-infra"
     }
   }
-
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
